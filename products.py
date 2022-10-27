a74bdd457f65ba702a1b3e7a1c96e436e8e871f4
Miguel_Roldan <miguel.roldantriana@gmail.com>
2022-10-27 15:18:29 -0500

    REST_API

---
+++ products.py
@@ -0,0 +1,6 @@
+products = [
+    {"name": "laptop", "price": 500.000, "quantity":6},
+    {"name": "mouse", "price": 90.000, "quantity":13},
+    {"name": "monitor", "price": 120.000, "quantity":3},
+    {"name": "iphone", "price":1200000,"quantity":15}
+]
